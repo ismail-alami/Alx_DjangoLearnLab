0ca677bb27f73aa59e3aead4b6d3b42c9da44e79
ismail-alami <alamismail1@gmail.com>
2024-08-13 23:40:23 +0100

    first commit

--- 0x1.Introduction_ to_ Django/LibraryProject/LibraryProject/settings.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/LibraryProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'practice_alx'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'LibraryProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'practice_alx',
+        'USER': 'ismail',
+        'PASSWORD': 'alxpassword',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
