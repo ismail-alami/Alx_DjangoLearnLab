d860b00bae04bcf6ec96d01fc9e12db8f4436a7d
ismail-alami <alamismail1@gmail.com>
2024-08-14 00:32:00 +0100

    add customization for the django admin interface

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -3,6 +3,6 @@ from .models import Book
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'published_date')
+    list_filter = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
 admin.site.register(Book, BookAdmin)
