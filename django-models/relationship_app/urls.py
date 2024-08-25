7abed8a1666d022576b8599a999e6700b27732e6
ismail-alami <alamismail1@gmail.com>
2024-08-25 22:19:20 +0100

    alx 3

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from .views import LoginView, LogoutView, RegisterView
+
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Book
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
 def list_books(request):
     books = Book.objects.all()
@@ -13,3 +17,17 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+# Login View - using Django's built-in view
+class LoginView(auth_views.LoginView):
+    template_name = 'login.html'
+
+# Logout View - using Django's built-in view
+class LogoutView(auth_views.LogoutView):
+    template_name = 'logout.html'
+
+# Registration View - using a custom view based on Django's UserCreationForm
+class RegisterView(CreateView):
+    template_name = 'register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
