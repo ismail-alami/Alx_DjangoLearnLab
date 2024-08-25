fb28638306387eb43f27cc9405809ccc9b506b97
ismail-alami <alamismail1@gmail.com>
2024-08-25 22:23:16 +0100

    alx 3

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,7 +7,8 @@ from .views import LoginView, LogoutView, RegisterView
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', register, name='register'),
+    
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render
 from .models import Book
-from django.contrib.auth import views as auth_views
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
-from django.contrib.auth import login
 
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 from django.views.generic import DetailView
-from django.views.generic.detail import DetailView
 from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
 # Login View - using Django's built-in view
 class LoginView(auth_views.LoginView):
@@ -28,7 +27,9 @@ class LogoutView(auth_views.LogoutView):
     template_name = 'logout.html'
 
 # Registration View - using a custom view based on Django's UserCreationForm
-class RegisterView(CreateView):
-    template_name = 'register.html'
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+def register(request):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('login')
+    return render(request, 'register.html', {'form': form})
