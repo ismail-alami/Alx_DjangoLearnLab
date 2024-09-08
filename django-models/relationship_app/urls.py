fb464f06ce3da7dbe931d24cc9f46e3c5f60c0a8
ismail-alami <alamismail1@gmail.com>
2024-09-08 17:42:43 +0100

    test2

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,7 @@
 # from django.contrib import admin  
 from django.urls import path
 from . import views  
+from .views import list_books
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
