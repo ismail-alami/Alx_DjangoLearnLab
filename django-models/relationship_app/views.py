4fe08d42a73c789b98629c9acd6ff81708ebbf03
ismail-alami <alamismail1@gmail.com>
2024-09-08 17:41:37 +0100

    test2

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Book, Library
 from .models import Library
 
